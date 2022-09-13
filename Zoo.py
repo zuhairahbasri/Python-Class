622660e01711d12860df997d457568dd603347d9
zuhairahbasri <85155910+zuhairahbasri@users.noreply.github.com>
2022-09-13 21:48:58 +0800

    Update Zoo.py

--- Zoo.py
+++ Zoo.py
@@ -3,11 +3,6 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns 
-import warnings 
-warnings.filterwarnings("ignore")
 
-import os
-print(os.listdir("../input"))
-
-class_ = pd.read_csv("../input/class.csv")
-zoo = pd.read_csv("../input/zoo.csv")
+zoo = pd.read_csv("zoo.csv")
+zoo.head()
