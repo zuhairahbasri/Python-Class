4d0d8468ca39fb564cd459e22e4c1f104e5fce02
zuhairahbasri <85155910+zuhairahbasri@users.noreply.github.com>
2022-09-13 22:07:23 +0800

    Update Zoo.py

--- Zoo.py
+++ Zoo.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
-import seaborn as sns 
 
 zoo = pd.read_csv("zoo.csv")
+zoo = pd.read_csv("https://github.com/zuhairahbasri/Python-Class/blob/main/Zoo.csv")
 st.title("Zoo Dataset")
-st.table(zoo)
+st.write(zoo) 
