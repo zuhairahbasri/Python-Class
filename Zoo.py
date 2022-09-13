ac1c12474a296eeb4e1c761680416b884069fc7a
zuhairahbasri <85155910+zuhairahbasri@users.noreply.github.com>
2022-09-13 22:15:15 +0800

    Update Zoo.py

--- Zoo.py
+++ Zoo.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-zoo = pd.read_csv("/zoo.csv")
+dataset_filepath = Path('/zoo.csv')
 st.title("Zoo Dataset")
 st.write(zoo) 
