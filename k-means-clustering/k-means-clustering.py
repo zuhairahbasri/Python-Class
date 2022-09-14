f32a0ed77c50b53f4bb985f82052873999d0f08f
zuhairahbasri <85155910+zuhairahbasri@users.noreply.github.com>
2022-09-14 11:54:53 +0800

    Update k-means-clustering.py

--- k-means-clustering/k-means-clustering.py
+++ k-means-clustering/k-means-clustering.py
@@ -1,4 +1,10 @@
+import pandas as pd
+import streamlit as st
+
 file = "/content/mall_customer (1).csv"
 
-import pandas as pd
-df = pd.read_csv(file)
+zoo = pd.read_csv("./k-means-clustering/mall_customer.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
+
+st.title("Hi, This is a dataset of Mall customers!")  # add a title
+st.write(zoo)  # visualize my dataframe in the Streamlit app
